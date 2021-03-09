6278960581d1285bbda6208b904ec829c0166830
agg-mohit <71784650+agg-mohit@users.noreply.github.com>
2021-03-09 15:05:33 +0530

    files uploaded

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+# avdmanager delete avd -n test
+# avdmanager create avd -n test -d pixel_3 -k "system-images;android-30;google_apis_playstore;x86"
+
+rm -rf /root/.android/avd/test.avd/*.lock
+emulator -avd test -no-window -noaudio -timezone Asia/Kolkata &
+# emulator @test -noaudio -timezone Asia/Kolkata &
+echo "Waiting 60s for AVD warm up"
+sleep 60
+echo "Installing APK"
+adb install app-debug.apk
+adb shell am start -n com.example.socketapp/com.example.socketapp.MainActivity
+echo "Finished installing & starting APK steps"
+sleep infinity
